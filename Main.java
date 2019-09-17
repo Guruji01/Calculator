d348797ef79d306675204f1affec283214e8a3a4
Admin <naveenkaushal441@gmail.com>
2019-09-17 09:39:13 +0530

    Added Main.java file

---
+++ Main.java
@@ -0,0 +1,27 @@
+package calculater;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("CalcView.fxml"));
+			Scene scene = new Scene(root,348,479);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
